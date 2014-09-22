f44dfe0886502546298f0e1cbceab5046c3d7557
sinisa <sinisa.ivkovic@sbgenomics.com>
2014-09-22 19:31:54 +0200

    run_job

--- executors/runner.py
+++ executors/runner.py
@@ -1,31 +1,41 @@
 import os
 import docker
 import logging
+import uuid
+import stat
 from docker.errors import APIError
+from cliche.adapter import Adapter, from_url
 
 
 class Runner(object):
 
     WORKING_DIR = '/work'
 
-    def __init__(self, working_dir='./', stdout=None, stderr=None):
+    def __init__(self, tool, working_dir='./', stdout=None, stderr=None):
         if not os.path.isabs(working_dir):
             working_dir = os.path.abspath(working_dir)
+        self.tool = tool
+        self.image_id = tool['requirements']['environment']['container']['imageId']
         self.working_dir = working_dir
         self.stdout = stdout
         self.stderr = stderr
 
-
     def run(self, command):
         pass
+
+    def run_job(self, job):
+        pass
+
+    def rnd_name(self):
+        return str(uuid.uuid4())
+        #return '-'.join([uuid.uuid4(), self.tool.get('description')]) get name from DOAP
 
 
 class DockerRunner(Runner):
 
-    def __init__(self, working_dir='./', image_id='ubuntu', dockr=None, stdout=None, stderr=None):
-        super(DockerRunner, self).__init__(working_dir, stdout, stderr)
+    def __init__(self, tool, working_dir='./', dockr=None, stdout=None, stderr=None):
+        super(DockerRunner, self).__init__(tool, working_dir, stdout, stderr)
         self.docker_client = dockr or docker.Client()
-        self.image_id = image_id
 
     def make_config(self, image, command):
         config = {'Image': image,
@@ -51,7 +61,7 @@ class DockerRunner(Runner):
 
     def is_success(self, container):
         self.wait(container)
-        return self.inspect()['State']['ExitCode'] == 0
+        return self.inspect(container)['State']['ExitCode'] == 0
 
     def get_stdout(self, container):
         self.wait(container)
@@ -63,15 +73,16 @@ class DockerRunner(Runner):
         return self.docker_client.logs(container, stdout=False, stderr=True,
                                        stream=False, timestamps=False)
 
-    def run(self, command):
+    def run(self, command, **kwargs):
         volumes = {self.WORKING_DIR: {}}
         binds = {self.working_dir: self.WORKING_DIR}
         config = self.make_config(self.image_id, command)
         config['Volumes'] = volumes
         config['WorkingDir'] = self.WORKING_DIR
+        config.update(**kwargs)
         try:
             cont = self.docker_client.create_container_from_config(config)
-        except APIError as e:
+        except APIError:
             logging.ERROR('Image %s not found:' % self.image_id)
         try:
             self.docker_client.start(container=cont, binds=binds)
@@ -79,19 +90,36 @@ class DockerRunner(Runner):
             logging.ERROR('Failed to run container')
         return cont
 
+    def run_job(self, job, job_id=None):
+        job_dir = job_id or self.rnd_name()
+        os.mkdir(job_dir)
+        os.chmod(job_dir, os.stat(job_dir).st_mode | stat.S_IROTH |
+                 stat.S_IWOTH | stat.S_IXOTH)
+        adapter = Adapter(self.tool)
+        container = self.run(['bash', '-c', adapter.cmd_line(job)],
+                             WorkingDir='/'.join([self.WORKING_DIR, job_dir]))
+        if self.is_success(container):
+            outputs = adapter.output_schema
+            for out in outputs['properties']:
+                outputs['properties'][out]['adapter']['glob'] = \
+                    '/'.join([job_dir, outputs['properties'][out]['adapter']['glob']])
+        else:
+            outputs = {"err": "%s" %self.get_stderr(container)}
+        return outputs
+
 
 class NativeRunner(Runner):
 
-    def __init__(self, working_dir='./', stdout=None, stderr=None):
-        super(NativeRunner, self).__init__(working_dir, stdout, stderr)
+    def __init__(self, tool, working_dir='./', stdout=None, stderr=None):
+        super(NativeRunner, self).__init__(tool, working_dir, stdout, stderr)
 
     def run(self, command):
         pass
 
 
 if __name__=='__main__':
-    command = ['bash', '-c', 'grep -r chr > output.txt']
-    runner = DockerRunner()
-    container = runner.run(command)
-    print runner.get_stdout(container)
-    print runner.get_stderr(container)
+    # command = ['bash', '-c', 'grep -r chr > output.txt']
+    doc = from_url(os.path.join(os.path.dirname(__file__), '../examples/bwa-mem.yml'))
+    tool, job = doc['tool'], doc['job']
+    runner = DockerRunner(tool)
+    print runner.run_job(job)
